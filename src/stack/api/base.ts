a9a2276b28b462e5042953dd11a3930ed0b9bb1f
Amitkanswal <amitkanswal7@gmail.com>
2024-07-17 15:10:18 +0530

    feat:recover adaptor

--- src/stack/api/base.ts
+++ src/stack/api/base.ts
@@ -2,7 +2,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import Query from './query';
 import { transform, addParam } from '../utils';
-import { dispatchPostRobotRequest } from "../../utils/adapter";
+import { dispatchPostRobotRequest } from "../../utils/adapter.ts";
 import { ApiRequestProps } from '../../types/stack.types';
 
 
